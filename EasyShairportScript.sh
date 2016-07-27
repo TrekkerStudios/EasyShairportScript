bd364308f18e352ca8bc413f07e5786e92617bc4
Tyler B <pupdad@users.noreply.github.com>
2016-07-26 21:08:39 -0400

    Create EasyShairportScript.sh

---
+++ EasyShairportScript.sh
@@ -0,0 +1,58 @@
+#!/bin/bash
+#Shairport execute with naming
+
+echo "Welcome to the Easy Shairport Script!"
+echo "Written by pupdad"
+echo " "
+echo "Leave this process running in the background and connect your device!"
+echo "Control+C to exit"
+
+echo " "
+echo " "
+
+#Checks for name save
+[ -e ~/spn.dat ] && action=0 || action=1
+
+#Finds value of action
+case $action in
+#If file exists
+ 0 )
+ #Checks for flag
+  while getopts 'r' flag; do
+   case "${flag}" in
+    r ) rename=true
+    ;;
+    * ) echo "Error, invalid flag!"
+        echo "Quitting..."
+        exit
+    ;;
+   esac
+  done
+  #Checks whether to rename
+  if [ "$rename" = true ] ; then
+   read -p "What should it be named?: " input
+   echo "Saving name" $input "..."
+   echo $input > ~/spn.dat
+   file="~/spn.dat"
+   name=$(cat "$file")
+   (cd ~/projects/airplay/shairport && exec shairport --name=$name)
+  else
+   file="~/spn.dat"
+   name=$(cat "$file")
+   (cd ~/projects/airplay/shairport && exec shairport --name=$name)
+  fi
+ ;;
+#If file doesn't exist
+ 1 )
+  echo "Name save not found."
+  echo " "
+  read -p "What should it be named?: " input
+  echo "Saving name" $input "..."
+  echo $input > ~/spn.dat
+  file="~/spn.dat"
+  name=$(cat "$file")
+  (cd ~/projects/airplay/shairport && exec shairport --name=$name)
+ ;;
+esac
+
+exit 0
